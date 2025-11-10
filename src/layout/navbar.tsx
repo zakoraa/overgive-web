c4883826e9dc1d9f58051004584de47b40ea7dcf
unknown <raflisilehu20@gmail.com>
2025-11-10 22:17:35 +0700

    ref(layout): customizing the navbar display

--- src/layout/navbar.tsx
+++ src/layout/navbar.tsx
@@ -6,7 +6,7 @@ import { LoginButton } from "@/modules/auth/login/components/login-button";
 
 const Navbar = async () => {
   return (
-    <nav className="fixed top-0 z-50 mx-auto flex w-full items-center justify-between gap-x-5 px-6 py-6 transition-all duration-300 md:left-1/2 md:w-auto md:-translate-x-1/2 md:justify-center md:px-0">
+    <nav className="fixed top-0 z-50 mx-auto flex w-full items-center justify-between gap-x-5 py-6 transition-all duration-300 md:left-1/2 md:w-auto md:-translate-x-1/2 md:justify-center md:px-0">
       <div className="bg-background mx-0 flex h-12 w-fit rounded-2xl border border-gray-400 px-7 shadow-sm">
         <Image
           className="min-w-26"
@@ -17,7 +17,7 @@ const Navbar = async () => {
         />
       </div>
 
-      <Dock className="bg-background text-foreground hidden flex-row items-center space-x-12 rounded-2xl border border-gray-400 px-8 text-sm whitespace-nowrap shadow-sm md:flex">
+      <Dock className="bg-background text-foreground hidden flex-row items-center space-x-16 rounded-2xl border border-gray-400 px-12 text-sm whitespace-nowrap shadow-sm md:flex">
         <DockIcon className="hover:text-primary">
           <Link href="/">Beranda</Link>
         </DockIcon>
