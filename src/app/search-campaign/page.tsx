0cf10de49c45dc2cfd692d027ed573dd9b68f9b6
unknown <raflisilehu20@gmail.com>
2025-12-08 21:35:31 +0700

    fix(seach-campaign): fix error when searching campaign

--- src/app/search-campaign/page.tsx
+++ src/app/search-campaign/page.tsx
@@ -1,13 +1,9 @@
 import { SearchCampaign } from "@/modules/search_campaigns";
 import { SearchCampaignProvider } from "@/modules/search_campaigns/providers/search-campaign-provider";
 
-export default function Page({
-  searchParams,
-}: {
-  searchParams: { q?: string };
-}) {
+export default function Page() {
   return (
-    <SearchCampaignProvider search={searchParams.q}>
+    <SearchCampaignProvider>
       <SearchCampaign />
     </SearchCampaignProvider>
   );
